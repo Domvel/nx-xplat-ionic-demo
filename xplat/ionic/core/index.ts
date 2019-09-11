5c7bc252bf246383db453a0f1abb20a534072544
Dominik Geng <dominikgeng@inovel.de>
2019-09-11 08:45:42 +0200

    Initial commit.

---
+++ xplat/ionic/core/index.ts
@@ -0,0 +1,1 @@
+export { CiIonicCoreModule } from './core.module';
---
+++ xplat/ionic/features/ui/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+import { HeaderBaseComponent } from '@my-apps/features';
+
+@Component({
+  selector: 'ci-ion-header',
+  templateUrl: 'header.component.html'
+})
+export class HeaderComponent extends HeaderBaseComponent {}
